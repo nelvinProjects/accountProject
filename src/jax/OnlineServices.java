0f36633a3be27245bd4413473886439e4ac0be24
nelvinProjects <nelvin.joseph@academytrainee.com>
2018-08-03 11:41:26 +0100

    No change

--- src/jax/OnlineServices.java
+++ src/jax/OnlineServices.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.google.gson.Gson;
 
@@ -75,4 +76,9 @@ public class OnlineServices {
 		return service.mapToJSON();
 	}
 	
+	@GET
+	@Path("/success")
+	public Response successSend() {
+		return Response.ok().build();
+	}
 }
